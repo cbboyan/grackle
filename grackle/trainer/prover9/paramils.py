6d0408e6fdb1cc99d06c741d07ba842876475c69
Jan Jakubuv <jakubuv@gmail.com>
2024-02-29 13:24:23 +0100

    chore: Removed old ParamILS trainer for Prover9

--- grackle/trainer/prover9/paramils.py
+++
@@ -1,9 +0,0 @@
-from ..paramils import ParamilsTrainer
-from . import domain
-
-class Prover9ParamilsTrainer(ParamilsTrainer):
-   
-   def domains(self, params):
-      return (domain.PARAMS % domain.DEFAULTS) + domain.CONDITIONS + domain.FORBIDDENS 
-   
-
